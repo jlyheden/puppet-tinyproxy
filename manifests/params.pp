71d215fed981bfe06bdf9e80fb3e930e43a66250
jlyheden <johan@lyheden.com>
2013-05-23 16:55:47 +0200

    updated params.pp to work in setups without osfamily fact

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,48 @@
 class tinyproxy::params {
   $autoupgrade = false
   case $::osfamily {
-    'Debian': {
+    'RedHat':{
+      $configfile = '/etc/tinyproxy/tinyproxy.conf'
+      $package = 'tinyproxy'
+      $service = 'tinyproxy'
+      $hasstatus = false
+      $hasrestart = true
+      $user = 'tinyproxy'
+      $group = 'tinyproxy'
+      $port = 8888
+      $listen = undef
+      $bind = undef
+      $bindsame = undef
+      $connection_timeout = 600
+      $allow = []
+      $errorfiles = {}
+      $defaulterrorfile= '/usr/share/tinyproxy/default.html'
+      $stathost = undef
+      $statfile = '/usr/share/tinyproxy/stats.html'
+      $logfile = '/var/log/tinyproxy/tinyproxy.log'
+      $syslog = undef
+      $log_level = 'Info'
+      $pidfile = '/var/run/tinyproxy/tinyproxy.pid'
+      $xtinyproxy = undef
+      $maxclients = 100
+      $minspareservers = 5
+      $maxspareservers = 20
+      $startservers = 10
+      $maxrequestsperchild = 0
+      $viaproxyname = 'tinyproxy'
+      $disableviaheader = undef
+      $filter = undef
+      $filterurls = undef
+      $filterextended = undef
+      $filtercasesensitive = undef
+      $filterdefaultdeny = undef
+      $anonymous = []
+      $connectport = []
+      $reverseonly = undef
+      $reversemagic = undef
+      $reversebaseurl = undef
+    }
+    default: {
       case $::lsbdistcodename {
         'lucid','precise': {
           $configfile = '/etc/tinyproxy.conf'
@@ -53,53 +94,9 @@ class tinyproxy::params {
           $reversebaseurl = undef
         }
         default: {
-          fail("Unsupported distro ${::lsbdistcodename}")
+          fail("Unsupported osfamily: ${::osfamily}, lsbdistcodename: ${::lsbdistcodename}")
         }
       }
-    }
-    'RedHat':{
-      $configfile = '/etc/tinyproxy/tinyproxy.conf'
-      $package = 'tinyproxy'
-      $service = 'tinyproxy'
-      $hasstatus = false
-      $hasrestart = true
-      $user = 'tinyproxy'
-      $group = 'tinyproxy'
-      $port = 8888
-      $listen = undef
-      $bind = undef
-      $bindsame = undef
-      $connection_timeout = 600
-      $allow = []
-      $errorfiles = {}
-      $defaulterrorfile= '/usr/share/tinyproxy/default.html'
-      $stathost = undef
-      $statfile = '/usr/share/tinyproxy/stats.html'
-      $logfile = '/var/log/tinyproxy/tinyproxy.log'
-      $syslog = undef
-      $log_level = 'Info'
-      $pidfile = '/var/run/tinyproxy/tinyproxy.pid'
-      $xtinyproxy = undef
-      $maxclients = 100
-      $minspareservers = 5
-      $maxspareservers = 20
-      $startservers = 10
-      $maxrequestsperchild = 0
-      $viaproxyname = 'tinyproxy'
-      $disableviaheader = undef
-      $filter = undef
-      $filterurls = undef
-      $filterextended = undef
-      $filtercasesensitive = undef
-      $filterdefaultdeny = undef
-      $anonymous = []
-      $connectport = []
-      $reverseonly = undef
-      $reversemagic = undef
-      $reversebaseurl = undef
-    }
-    default: {
-      fail("Unsupported distro ${::osfamily}")
     }
   }
 }
